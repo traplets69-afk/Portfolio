dad94a6898da5fc2ed668869d59d3942f74b64b4
Krystian Suarez <traplets69@gmail.com>
2026-01-17 00:22:48 +0800

    Update portfolio with new changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
-      <div className="text-white-500 flex gap-2">
+   <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+     {/*  <div className="text-white-500 flex gap-2">
         <p>Terms & Conditions</p>
         <p>|</p>
         <p>Privacy Policy</p>
@@ -19,7 +19,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <p className="text-white-500">© 2024 Adrian Hajdin. All rights reserved.</p>
+      {/*<p className="text-white-500">© 2026 Adrian Hajdin. All rights reserved.</p>*/}
     </footer>
   );
 };
